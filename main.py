29b942d3b98e7949687866188a438d4f2391cdcd
Lewis Lee <lewis.patrick.lee@gmail.com>
2023-03-27 23:24:07 +0100

    Changing it to a RNN

--- main.py
+++ main.py
@@ -79,13 +79,11 @@ encoder.adapt(raw_train_ds.map(lambda text, label: text))
 
 model = tf.keras.Sequential([
     encoder,
-    tf.keras.layers.Embedding(
-        input_dim=len(encoder.get_vocabulary()),
-        output_dim=64,
-        # Use masking to handle the variable sequence lengths
-        mask_zero=True),
-    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
+    tf.keras.layers.Embedding(len(encoder.get_vocabulary()), 64, mask_zero=True),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, return_sequences=True)),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
     tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
     tf.keras.layers.Dense(1)
 ])
 
@@ -95,7 +93,7 @@ model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
 
 history = model.fit(raw_train_ds,
                     epochs=5,
-                    batch_size=64,
+                    batch_size=32,
                     validation_data=raw_val_ds,
                     validation_steps=30)
 
