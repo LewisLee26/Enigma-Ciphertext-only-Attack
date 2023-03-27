9a6981de73fa84da0556fc0e2516e596f77e5147
Lewis Lee <lewis.patrick.lee@gmail.com>
2023-03-27 23:26:43 +0100

    Removing unused files

--- enigmaData.py
+++ enigmaData.py
@@ -1,28 +1,33 @@
 import enigma
 import random
 import re
+import os
+
+path = os.getcwd()
 
 regex = re.compile('[^a-z]')
 
-rotor1 = enigma.Rotor([],0)
-rotor2 = enigma.Rotor([],0)
-rotor3 = enigma.Rotor([],0)
+rotor1 = enigma.Rotor([], 0)
+rotor2 = enigma.Rotor([], 0)
+rotor3 = enigma.Rotor([], 0)
 
 plugboard1 = enigma.Plugboard([])
 
 reflector1 = enigma.Reflector([])
 
+
 def encryptText(i):
     rotor1.randomizeCharacterPairs()
-    rotor1.setRotorPosition(random.randint(0,25))
+    rotor1.setRotorPosition(random.randint(0, 25))
     rotor2.randomizeCharacterPairs()
-    rotor2.setRotorPosition(random.randint(0,25))
+    rotor2.setRotorPosition(random.randint(0, 25))
     rotor3.randomizeCharacterPairs()
-    rotor3.setRotorPosition(random.randint(0,25))
+    rotor3.setRotorPosition(random.randint(0, 25))
     plugboard1.randomizeCharacterPairs()
     reflector1.randomizeCharacterPairs()
     print(i)
-    return enigma.enigma(content[i*250:(i+1)*250],rotor1,rotor2,rotor3,plugboard1,reflector1)
+    return enigma.enigma(content[i * size:(i + 1) * size], rotor1, rotor2, rotor3, plugboard1, reflector1)
+
 
 with open("shakespeare.txt") as f:
     content = f.read()
@@ -30,33 +35,34 @@ with open("shakespeare.txt") as f:
 f.close()
 
 decryptedText = regex.sub('', content.lower())
+size = 1000
+trainSplit = 0.7
+testSplit = 1 - trainSplit
 
-with open("decrypted_train.txt", "w") as f:
-    f.write(decryptedText[:3125000])
+# decrypted_train
+for i in range(int(((len(decryptedText)*trainSplit) // size) - 1)):
+    with open(path+"/dataset/train/decryption/" + str(i) + ".txt", "w") as f:
+        f.write(decryptedText[i * size:(i + 1) * size])
+    f.close()
+    print(i)
 
-f.close()
+# decrypted_test
+for i in range(int(((len(decryptedText)*testSplit)// size) - 1)):
+    with open(path+"/dataset/test/decryption/" + str(i) + ".txt", "w") as f:
+        f.write(decryptedText[int((i + (len(decryptedText)*testSplit // size) - 1) * size):int((i + (len(decryptedText)*testSplit // size)) * size)])
+    f.close()
+    print(i + len(decryptedText)*testSplit)
 
+# encrypted_train
+for i in range(int((len(decryptedText)*trainSplit)//size)):
+    with open(path+"/dataset/train/encryption/" + str(i) + ".txt", "w") as f:
+        f.write(encryptText(i))
+    f.close()
+    print(i)
 
-with open("decrypted_test.txt", "w") as f:
-    f.write(decryptedText[3125000:])
-
-f.close()
-
-output = ""
-for i in range (12500):
-    output += encryptText(i)
-
-with open("encrypted_train.txt", "w") as f:
-    f.write(output)
-
-f.close()
-
-output = ""
-for i in range (2500 - 1):
-    output += encryptText(i+12500)
-
-with open("encrypted_test.txt", "w") as f:
-    f.write(output)
-
-f.close()
-
+# encrypted_test
+for i in range(int(((len(decryptedText)*testSplit)//size)-1)):
+    with open(path+"/dataset/test/encryption/" + str(i) + ".txt", "w") as f:
+        f.write(encryptText(i + int(len(decryptedText)*testSplit)//size))
+    f.close()
+    print(i + (len(decryptedText)*testSplit)//size)
